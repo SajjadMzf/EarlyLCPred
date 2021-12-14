c510fd95e582d9f4577397a70275cc01c429a124
Sajjad <sajjad.mozaffari@warwick.ac.uk>
2021-12-14 19:03:35 +0000

    refactored main and param files

--- data_prep/main.py
+++ data_prep/main.py
@@ -8,146 +8,82 @@ from render_scenarios import RenderScenarios
 import param as p
 
 
-def extract(core_num, file_row, track_path, pickle_path_frame, pickle_path_tracks, meta_path, static_path, dataset_name):
-    np.random.seed(0)
-    if isinstance(track_path, str):
-        i = 0
-        file_num = file_row + i + 1
-        print('Core number: ', core_num, ' started on file number: ', file_num)
-        start = time.time()
+def extract(core_num, file_numbers):
+    start = time.time()
+    for file_number in file_numbers:
+        print('Core number: ', core_num, ' started on file number: ', file_number)
+        
         extractor = ExtractScenarios(
-            file_num,
-            track_path, 
-            pickle_path_tracks,
-            pickle_path_frame, 
-            static_path,
-            meta_path,
-            dataset_name)
-        extractor.extract_and_save()
-        
-            
-        end = time.time()
-        print('Core Number: ', core_num, ' Ended in: ', end-start, ' s.')
-    else:
-        start = time.time()
-        for i in range(len(track_path)):
-            file_num = file_row + i + 1
-            print('Core number: ', core_num, ' started on file number: ', file_num)
-            
-            extractor = ExtractScenarios(
-                file_num,
-                track_path[i], 
-                pickle_path_tracks[i],
-                pickle_path_frame[i], 
-                static_path[i],
-                meta_path[i],
-                dataset_name)
-            extractor.extract_and_save() 
-        
-        end = time.time()
-        print('Core Number: ', core_num, ' Ended in: ', end-start, ' s.')
+            file_number,
+            p.track_paths[file_number], 
+            p.track_pickle_paths[file_number],
+            p.frame_pickle_paths[file_number], 
+            p.static_paths[file_number],
+            p.meta_paths[file_number],
+            p.DATASET)
+        extractor.extract_and_save() 
+    
+    end = time.time()
+    print('Core Number: ', core_num, ' Ended in: ', end-start, ' s.')
 
-def render(core_num, file_row, track_path, pickle_path_frame, pickle_path_tracks, meta_path, static_path, dataset_name):
-    np.random.seed(0)
-    if isinstance(track_path, str):
-        i = 0
-        file_num = file_row + i + 1
-        print('Core number: ', core_num, ' started on file number: ', file_num)
+def render(core_num, file_numbers):
+    for file_number in file_numbers:
+        print('Core number: ', core_num, ' started on file number: ', file_number)
         start = time.time()
+        
         renderer = RenderScenarios(
-            file_num,
-            track_path, 
-            pickle_path_frame, 
-            static_path,
-            meta_path,
-            dataset_name
-        ) 
+            file_number,
+            p.track_paths[file_number], 
+            p.frame_pickle_paths[file_number], 
+            p.static_paths[file_number],
+            p.meta_paths[file_number],
+            p.DATASET
+        )
+        renderer.update_dirs() 
         renderer.load_scenarios()
         renderer.render_scenarios()
-        renderer.save_dataset() 
-        end = time.time()
-        print('Core Number: ', core_num, ' Ended in: ', end-start, ' s.')
-    else:
-        for i in range(len(track_path)):
-            file_num = file_row + i + 1
-            print('Core number: ', core_num, ' started on file number: ', file_num)
-            start = time.time()
-            
-            renderer = RenderScenarios(
-                file_num,
-                track_path[i], 
-                pickle_path_frame[i], 
-                static_path[i],
-                meta_path[i],
-                dataset_name
-            )
-            renderer.update_dirs() 
-            renderer.load_scenarios()
-            renderer.render_scenarios()
-            renderer.save_dataset()  
+        renderer.save_dataset()  
 
-            end = time.time()
-        print('Core Number: ', core_num, ' Ended in: ', end-start, ' s.')
+        end = time.time()
+    print('Core Number: ', core_num, ' Ended in: ', end-start, ' s.')
 
 
 
 
 if __name__ =="__main__":
     
+    np.random.seed(0)   
+    # Single Core (For Debugging purposes)
     
-    if  p.DATASET != "HIGHD":
-        for i in range(p.file_numbers):
-            extract(i, i, p.track_paths[i], p.frame_pickle_paths[i], p.track_pickle_paths[i], p.meta_paths[i], p.static_paths[i], p.DATASET)
-            render(i, i, p.track_paths[i], p.frame_pickle_paths[i], p.track_pickle_paths[i], p.meta_paths[i], p.static_paths[i], p.DATASET)
-        
-    else:
-        
-        # Single Core (For Debugging purposes)
-        '''
-        i = 26
-        #extract(i, i, p.track_paths[i], p.frame_pickle_paths[i], p.track_pickle_paths[i], p.meta_paths[i], p.static_paths[i],  p.DATASET)    
-        render(i, i, p.track_paths[i], p.frame_pickle_paths[i], p.track_pickle_paths[i], p.meta_paths[i], p.static_paths[i],  p.DATASET)
-        exit()
-        '''
+    i = np.array([26])
+    extract(1, i)    
+    render(1, i)
+    exit()
+    
 
-        # Extract LC Scenarios 
-        total_cores = 3
-        total_files = p.file_numbers - p.start_ind
-        file_per_core = int(total_files/total_cores)
-        procs = []
-        for core_num in range(total_cores):
-            file_row = file_per_core*core_num + p.start_ind
-            
-            proc = Process(target= extract, args = ( 
-                                                core_num+1,
-                                                file_row, 
-                                                p.track_paths[file_row:(file_row+file_per_core)], 
-                                                p.frame_pickle_paths[file_row:(file_row+file_per_core)], 
-                                                p.track_pickle_paths[file_row:(file_row+file_per_core)],
-                                                p.meta_paths[file_row:(file_row+file_per_core)], 
-                                                p.static_paths[file_row:(file_row+file_per_core)],
-                                                p.DATASET))
-            procs.append(proc)
-            proc.start()
-        
-        for proc in procs:
-            proc.join()
+    # Extract LC scenarios (multi-threads)
+    total_cores = 3
+    file_numbers = np.arange(1,61)
+    total_files = len(file_numbers)
+    file_per_core = int(total_files/total_cores)
+    procs = []
+    for core_num in range(total_cores):
+        file_row = file_per_core*core_num
+        core_fle_numbers = file_numbers[file_per_core*core_num:(file_per_core*(core_num+1))]
+        proc = Process(target= extract, args = (core_num+1, core_fle_numbers))
+        procs.append(proc)
+        proc.start()
+    
+    for proc in procs:
+        proc.join()
 
-        # Render Whole Dataset Using Multi-threads
-        for core_num in range(total_cores):
-            file_row = file_per_core*core_num + p.start_ind
-            
-            proc = Process(target= render, args = ( 
-                                                core_num+1,
-                                                file_row, 
-                                                p.track_paths[file_row:(file_row+file_per_core)], 
-                                                p.frame_pickle_paths[file_row:(file_row+file_per_core)], 
-                                                p.track_pickle_paths[file_row:(file_row+file_per_core)],
-                                                p.meta_paths[file_row:(file_row+file_per_core)], 
-                                                p.static_paths[file_row:(file_row+file_per_core)],
-                                                p.DATASET))
-            procs.append(proc)
-            proc.start()
-        
-        for proc in procs:
-            proc.join()
+    # Render extracted LC scenarios (multi-threads)
+    for core_num in range(total_cores):
+        file_row = file_per_core*core_num
+        core_fle_numbers = file_numbers[file_per_core*core_num:(file_per_core*(core_num+1))]
+        proc = Process(target= render, args = (core_num+1, core_fle_numbers))
+        procs.append(proc)
+        proc.start()
+    
+    for proc in procs:
+        proc.join()
